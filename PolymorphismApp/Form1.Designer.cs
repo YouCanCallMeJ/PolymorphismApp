c215affefedd73dc98d916ae10e8941c9e158b9e
Sabbir Ahmed <sahmed@conestogac.on.ca>
2021-10-06 11:11:04 -0400

    ui designed

--- PolymorphismApp/Form1.Designer.cs
+++ PolymorphismApp/Form1.Designer.cs
@@ -29,13 +29,69 @@ namespace PolymorphismApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(91, 53);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(210, 44);
+            this.btnRectangle.TabIndex = 0;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(91, 159);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(210, 44);
+            this.btnCircle.TabIndex = 0;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(91, 278);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(210, 44);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.Location = new System.Drawing.Point(91, 400);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(347, 23);
+            this.btnTriangle.TabIndex = 1;
+            this.btnTriangle.Text = "Triangle (future feature)";
+            this.btnTriangle.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(885, 510);
+            this.Controls.Add(this.btnTriangle);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnRectangle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnTriangle;
     }
 }
 
